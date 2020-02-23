04139eee6f7a4c70b09d0e1e832654eb1072c2e0
yulijun <koalaylj@gmail.com>
2020-02-23 17:50:09 +0800

    修复html没有拷贝的问题

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,38 @@
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 
 module.exports = {
-  entry: ['./src/js/main.js'],
+  context: path.join(__dirname, 'src'),
+  entry: ['./js/main.js'],
   // mode: 'none', // none development production
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'game.min.js',
+    filename: 'game.min.[hash:8].js',
   },
-  target: 'web'
+  target: 'web',
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'assets/',to:'assets/'}
+    ], {
+      ignore: [],
+      debug:'debug',
+      copyUnmodified: true
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i ,
+      // optipng: {
+      //   optimizationLevel: 4
+      // },
+      pngquant: {
+        verbose:true,
+        quality: '80-90',
+      }
+    })
+    ,new HtmlPlugin({
+      file:path.join(__dirname,'dist','index.html'),
+      template:'./index.html'
+    })
+  ]
 }
